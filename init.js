653acbd5069bd3733093ec6db4db51ff1da452f5
OleHenFo <olehenfo@stud.ntnu.no>
2019-04-12 01:33:19 +0200

    edits

---
+++ init.js
@@ -0,0 +1,6 @@
+const Discordbot = require('./discord/discordbot.js');
+const Twitchbot = require('./twitch/twitchbot.js');
+const Misc = require('./misc/misc.js');
+
+Discordbot.init();
+Twitchbot.init();
